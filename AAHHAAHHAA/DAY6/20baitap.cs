1389d393de793361b211ea4f812ee25e07b20e73
Nguyễn Tấn Bổng <tanbong210@gmail.com>
2026-09-17 15:09:20 +0700

    cslt buoi 6 5/20

---
+++ AAHHAAHHAA/DAY6/20baitap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AAHHAAHHAA.DAY6
+{
+    public class btvn
+    {
+        static int TinhTong(int a, int b) //TinhTong
+        {
+            int tinhtong = a + b;
+            return tinhtong;
+        }
+        static bool KiemTraChan(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        static int TimMax (int a, int b, int c)
+        {
+            int max = Math.Max(a, b);
+            max = Math.Max(max, c);
+            return max;
+        }
+        static long TinhGiaiThua (int n)
+        {
+            long factorial = 1;
+            for (int i = 1;i<=n;i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+        static string DaoNguocChuoi (string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string (charArray);
+        }
+        static void Main(string[] args)
+        {
+
+        }
+        
+    }
+}
--- AAHHAAHHAA/DAY6/baitapbuoi6.cs
+++ AAHHAAHHAA/DAY6/baitapbuoi6.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace AAHHAAHHAA.DAY6
@@ -37,25 +38,113 @@ namespace AAHHAAHHAA.DAY6
                 factorial = i * factorial;
                 
             }
-            Console.WriteLine($"Factorial of your num = {factorial}");
             return factorial;
         }
-        static bool isPrime (int a)
+        static bool isPrime (int a) //Tìm prime
         {
-            if (a <= 1)
-            {
-                Console.WriteLine("Nhập số lớn hơn 1");
-            }
+            if (a <= 1) { return false;}
+            if (a == 2) { return true; }
+            if (a%2 == 0) { return false;}
             else
             {
-
+                for (int i = 3; i*i <= a; i++)
+                {
+                    if (a%i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
             }
-
+        }
+        static string primeUnder (int a) //Tìm prime bé hơn N
+        {
+            string listprime = "2";
+            if (a < 2)
+            {
+                Console.WriteLine("Nhập số lớn hơn");
+                return "";
+            }
+            if (a == 2) { return "2"; }
+            else
+            {
+               for (int j = 3; j < a; j+=2)
+                {
+                    if (isPrime (j))
+                    {
+                        listprime += $", {j}";
+                    }
+                }
+                return listprime;
+            }
+          
+        }
+        static string Nprime (int a) //Tìm n prime đầu tiên
+        {
+            if (a < 1) return "";
+            if (a == 1) return "2";
+                string listprime2 = "2";;
+            if (a>1)
+            {
+                int startNum = 3;
+                int count = 1;
+                while (count<=a-1)
+                {
+                    if (isPrime (startNum))
+                    {
+                        listprime2 += $", {startNum}";
+                        count++;
+                    }
+                    startNum += 2;
+                }
+            }
+            return listprime2;
+        }
+        static bool checkPerfect (int a)
+        {
+            if (a<=1) return false;
+            int Uoc = 1;
+               for ( int j = 2; j*j < a; j++)
+                {
+                    if (a%j==0)
+                    {
+                       Uoc += j;
+                        if (j*j != a)
+                        {
+                        Uoc += a / j;
+                        }
+                    }
+                }
+          return a == Uoc;
+        }
+        static string listPerfect (int a)
+        {
+            List<int> result = new List<int>();
+            for ( int i = 2; i < a;i+=2)
+            {
+                if (checkPerfect (i))
+                {
+                    result.Add(i);
+                }
+            }
+            return string.Join (",", result);
+        }
+        static bool checkPangrams (string input)
+        {
+            string lowerinput = input.ToLower();
+            for (char c = 'a'; c <= 'z';c++)
+            {
+                if (!lowerinput.Contains (c)) 
+                {  
+                    return false; 
+                }
+            } 
             return true;
         }
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
-            
+            string halo = "The quick brown fox jumps over the lazy dog";
+            Console.WriteLine($" {halo} {(checkPangrams(halo) ? "Là pangrams" : "Không phải pangrams")} ");
         }
     }
 }
